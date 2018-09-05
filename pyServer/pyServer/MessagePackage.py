3a410d8aa2e51b06b4c748a5c972868cc87b63e9
SteakingCoder <1941499609@qq.com>
2018-09-05 09:54:02 +0800

    添加MessagePackage成员

--- pyServer/pyServer/MessagePackage.py
+++ pyServer/pyServer/MessagePackage.py
@@ -1,12 +1,20 @@
 import simplejson
+import random
+import uuid
+import time
+
+
+node = uuid.getnode()
 
 
 class MessagePackage:
     mtype = ""
     host_name = ""
+    host_id = ""
     memery = ""
     cpu = ""
     process = ""
+    timestamp = ""
 
     def __init__(self, mtype, host_name, memery, cpu, process):
         self.mtype = mtype
@@ -14,7 +22,18 @@ class MessagePackage:
         self.memery = memery
         self.cpu = cpu
         self.process = process
+        self.generate_hostuuid()
+        self.generate_timestamp()
 
     def to_json(self):
-        dictionary = {'type': self.mtype, 'host_name': self.host_name, 'cpu': self.cpu, 'memery': self.memery, 'process': self.process}
+        dictionary = {'type': self.mtype, 'time': self.timestamp,
+                      'hostid': self.host_id, 'hostname': self.host_name,
+                      'cpu': self.cpu, 'mem': self.memery, 'process': self.process}
         return simplejson.load(dictionary)
+
+    def generate_hostuuid(self):
+        self.host_id = uuid.uuid1(node+(random.random()*100000000))
+
+    def generate_timestamp(self):
+        localtime = time.localtime()
+        self.timestamp = '%02d' % localtime[3] + '-%02d' % localtime[4] + '-%02d' % localtime[5]
